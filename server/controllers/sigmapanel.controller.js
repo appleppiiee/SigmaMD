ed5551fd14475b3aa1b825b73b68db562c69bff0
appleppiiee <tacardon.apple@gmail.com>
2025-03-25 15:46:34 -0400

    sigmapanel followup

---
+++ server/controllers/sigmapanel.controller.js
@@ -0,0 +1,66 @@
+import Sigmapanel from '../models/sigmapanel.model.js'
+import extend from 'lodash/extend.js'
+import errorHandler from './error.controller.js'
+const create = async (req, res) => { 
+const sigmapanel = new Sigmapanel(req.body) 
+try {
+await sigmapanel.save()
+return res.status(200).json({ 
+message: "Successfully Created!"
+})
+} catch (err) {
+return res.status(400).json({
+error: errorHandler.getErrorMessage(err) 
+})
+} 
+}
+const list = async (req, res) => { 
+try {
+let sigmapanels = await Sigmapanel.find().select('encounterID patientID visitDateTime vHeight vWeight vBmi vTemp vBloodPressure vPulseRate mhMedicalHistory mhFamilyHistory mhSocialHistory mhAllergies mhCurrentMedications diagnosis plMedication plReferrals plFollowup plProcedures notes') 
+res.json(sigmapanels)
+} catch (err) {
+return res.status(400).json({
+error: errorHandler.getErrorMessage(err) 
+})
+} 
+}
+const sigmapanelByID = async (req, res, next, id) => { 
+try {
+let sigmapanel = await Sigmapanel.findById(id) 
+if (!sigmapanel)
+return res.status(400).json({ 
+error: "sigmapanel not found"
+})
+req.sigmapanel = sigmapanel 
+next()
+} catch (err) {
+return res.status('400').json({ 
+error: "Could not retrieve sigmapanel"
+}) 
+}
+}
+const read = (req, res) => {
+//req.profile.hashed_password = undefined 
+//req.profile.salt = undefined
+return res.json(req.sigmapanel) 
+}
+const update = async (req, res) => { 
+try {
+let sigmapanel = req.sigmapanel
+sigmapanel = extend(sigmapanel, req.body) 
+sigmapanel.updated = Date.now() 
+await sigmapanel.save()
+//project.hashed_password = undefined 
+//project.salt = undefined
+res.json(sigmapanel) 
+} catch (err) {
+return res.status(400).json({
+error: errorHandler.getErrorMessage(err) 
+})
+} 
+}
+
+
+export default { create, sigmapanelByID, read, list, update }
+
+
---
+++ server/models/sigmapanel.model.js
@@ -0,0 +1,109 @@
+import mongoose from 'mongoose'
+//const mongoose = require('mongoose');
+const SigmapanelSchema = new mongoose.Schema({
+
+    encounterID: {
+        type: String,
+        trim: true,
+        required: 'EncounterID is required'
+    },  
+    patientID: {
+        type: String,
+        trim: true,
+        required: 'PatientID is required'
+    }, 
+    userID: {
+        type: String,
+        trim: true,
+        required: 'ProviderID is required'
+    },
+    visitDateTime: {
+        type: Date,
+        default: Date.now   
+    },
+    vHeight: {
+        type: String,
+        trim: true,
+        required: 'Height is required'
+        },
+    vWeight: {
+        type: String,
+        trim: true,
+        required: 'Weight is required'
+    },
+    vBmi: {
+        type: String,
+        trim: true,
+        required: 'BMI is required'
+    },
+    vTemp: {
+        type: String,
+        trim: true,
+        required: 'Temperature is required'
+    },  
+    vBloodPressure: {
+        type: String,
+        trim: true,
+        required: 'Blood Pressure is required'
+    },
+    vPulseRate: {
+        type: String,
+        trim: true,
+        required: 'Pulse Rate is required'
+    },
+    mhMedicalHistory: {
+        type: String,
+        trim: true
+    },
+    mhFamilyHistory: {
+        type: String,
+        trim: true
+    },
+    mhSocialHistory: {
+        type: String,
+        trim: true
+    },
+     mhAllergies: {
+        type: String,
+        trim: true
+    },
+    mhCurrentMedications: {
+        type: String,
+        trim: true
+    },
+    diagnosis: {
+        type: String,
+        trim: true,
+        required: 'Diagnosis is required'
+    },
+    plMedication: {
+        type: String,
+        trim: true
+    },
+    plReferrals: {
+        type: String,
+        trim: true
+    },
+    plFollowup: {
+        type: String,
+        trim: true
+    },
+    plProcedures: {
+        type: String,
+        trim: true
+    },
+    notes: {
+        type: String,
+        trim: true,
+        required: 'Additional Notes is required'
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now   
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now   
+    }                                  
+ });
+export default mongoose.model('Sigmapanel', SigmapanelSchema);
